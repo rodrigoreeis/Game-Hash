6e5439a63e5f2e4e34a66facf8233dafc72e6b0e
Rodrigo Reis <roreis99@gmail.com>
2019-09-28 16:33:20 -0700

    feat: objects menu

---
+++ src/objects/Menu/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './styles.css';
+
+const Menu = () => (
+  <a href="#" className="menu">
+    <span className="center">Menu</span>
+  </a>
+)
+export default Menu;
