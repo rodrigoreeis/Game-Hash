22b97659bc56748284ded7e3d599136ae2609204
Rodrigo Reis <roreis99@gmail.com>
2019-09-28 18:44:54 -0700

    feat: grid players

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
+import './App.css';
 import Header from './components/Header';
-import Card from './objects/Card';
-import Player from './objects/Player';
+import Hashtag from './components/Hashtag';
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Card> 
-        <Player player='x'/>
-        <Player player='o'/>
-        <Player player='x'/>
-        <Player player='o'/>
-        <Player player='x'/>
-        <Player player='o'/>
-        <Player player='x'/>
-        <Player player='o'/>
-        <Player player='x'/>
-      </Card>
-    </>
-  );
-}
+const  App = () => (
+  <main className="app">
+    <Header />
+    <Hashtag />
+  </main>
+);
 
 export default App;
