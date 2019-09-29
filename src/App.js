2c4c8709615198b36e002263119defd3a82049a1
Rodrigo Reis <roreis99@gmail.com>
2019-09-29 20:26:55 -0300

    feat: create object IconClose

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import About from './objects/About';
 import Header from './components/Header';
 import Hashtag from './components/Hashtag';
 
-import RodrigoPhoto from './img/rodrigo.jpeg';
 import AboutLink from './objects/AboutLink';
+import IconClose from './objects/IconClose';
 
 const  App = () => (
   <main className="app">
@@ -16,6 +16,7 @@ const  App = () => (
     <CheckBox id="show" value="show" content="Mostrar Eventos"/>
     <About >
       <AboutLink className="-light" />
+      <IconClose />
     </About>
   </main>
 );
---
+++ src/objects/IconClose/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const IconClose = () => {
+  return (  
+    <a href="#" className="icon-close">Fechar</a>
+  );
+};
+ 
+export default IconClose;
