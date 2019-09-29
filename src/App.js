e62591b7f821611f2e6d013ebb1b1b339b7a054c
Rodrigo Reis <roreis99@gmail.com>
2019-09-28 17:16:56 -0700

    feat: player object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import Card from './objects/Card';
+import Player from './objects/Player';
 
 function App() {
   return (
     <>
       <Header />
       <Card />
+      <Player />
     </>
   );
 }
---
+++ src/objects/Player/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const Player = () => (
+  <button className="player">X</button>
+);
+
+export default Player;
