9e03d60558e9dc8ef8918f8767b7e1c90d6259a0
Rodrigo Reis <roreis99@gmail.com>
2019-09-29 23:02:49 -0300

    feat: create component ProfileUser

--- src/components/ProfileUser/index.js
+++ src/components/ProfileUser/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
+import './styles.css';
+
+import Github from '../../img/github-icon.png';
+import Facebook from '../../img/facebook-icon.png';
+import Linkedin from '../../img/linkedin-icon.png';
+import Twitter from '../../img/twitter-icon.png';
+import Instagram from '../../img/instagram-icon.png';
+
 import AvatarProfile from '../../objects/AvatarProfile';
 import Profile from '../../img/profile.jpeg';
+import IconSocial from '../../objects/IconSocial';
 
 const ProfileUser = () => {
   return ( 
-    <dl>
-      <dd>
+    <dl className="profile-user">
+      <dd className="avatar">
         <AvatarProfile src={Profile} />
       </dd>
-      <dt> Rodrigo Reis </dt>
-      <dd>Desenvolvedor Front-end com sólida experiência em e-commerce. Fascinado por VanillaJS. Expert em acessibilidade e entusiasta UX/UI.</dd>
+      <dt className="title"> Rodrigo Reis </dt>
+      <dd className="description"> Desenvolvedor Front-end com sólida experiência em e-commerce. Fascinado por VanillaJS. Expert em acessibilidade e entusiasta UX/UI.</dd>
+      <dd className="socials">
+        <IconSocial src={Github} alt= "logo do Github" />
+        <IconSocial src={Facebook} alt= "logo do Facebook" />
+        <IconSocial src={Linkedin} alt= "logo do Likedin" />
+        <IconSocial src={Twitter} alt= "logo do Twitter" />
+        <IconSocial src={Instagram} alt= "logo do Instagram" />
+      </dd>
     </dl>
   );
-}
+};
  
 export default ProfileUser;
---
+++ src/objects/IconSocial/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const IconSocial = ({ src, alt }) => {
+  return ( 
+    <img className="icon-social" src={src} alt={alt} />
+  );
+}
+ 
+export default IconSocial;
